c8a0716d5328f183393af009b8bf8f82e683c002
Samundishwari <samuprithi@gmail.com>
2025-10-25 19:44:33 +0530

    Time: 0 ms (100%), Space: 12.5 MB (76.13%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def threeConsecutiveOdds(self, arr):
+        count = 0
+        for i in arr:
+            if (i % 2 == 0):
+                count = 0
+            else:
+                count += 1
+                if count >= 3:
+                    return True
+
+        return False
