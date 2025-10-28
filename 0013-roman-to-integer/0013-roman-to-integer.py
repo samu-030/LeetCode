4617476757dbaec35db1b386b51fbfeefca8dcc3
Samundishwari <samuprithi@gmail.com>
2025-10-28 22:40:22 +0530

    Time: 14 ms (27.29%), Space: 12.4 MB (52.84%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def romanToInt(self, s):
+        roman_dict = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        total = 0
+
+        for i in range(len(s)-1):
+            if roman_dict[s[i]] < roman_dict[s[i+1]]:
+                total -= roman_dict[s[i]]
+            else:
+                total += roman_dict[s[i]]
+
+        total += roman_dict[s[-1]]
+
+        return total
+
+       
