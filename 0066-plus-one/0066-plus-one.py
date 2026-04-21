95f072c88ab4ecde0dd21c934edc9c44a955036f
Samundishwari <samuprithi@gmail.com>
2026-04-21 21:49:05 +0530

    Time: 0 ms (100%), Space: 12.3 MB (90.91%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def plusOne(self, digits):
+        n = len(digits)
+
+        for i in range(n-1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+
+            else:
+                digits[i] += 1
+                return digits
+
+        return [1] + digits
+        '''n = len(digits)
+
+        digits[n-1] += 1
+        if digits[n-1] == 9:
+            digits[n-1-1] += 1
+
+        return digits
+        '''
