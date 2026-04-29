9073f37f088c3cf2e2d84e9f1ca27f580a97dee2
Samundishwari <samuprithi@gmail.com>
2026-04-29 14:20:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3895-count-digit-appearances/3895-count-digit-appearances.py
+++ 3895-count-digit-appearances/3895-count-digit-appearances.py
@@ -8,3 +8,17 @@ class Solution(object):
                 if ch == target:
                     count += 1
         return count
+
+        '''for i in nums:
+            if i > 9:
+                rem = i % 10
+                i = i // 10
+
+                if rem == digit:
+                    count += 1
+
+            if i == digit:
+                count += 1
+
+        return count
+        '''
