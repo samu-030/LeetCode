c8c52717330044e8d4554bee1c5074e9428723d6
Samundishwari <samuprithi@gmail.com>
2026-06-16 12:52:18 +0530

    Time: 42 ms (43.87%), Space: 12.6 MB (55.59%) - LeetHub

--- 3895-count-digit-appearances/3895-count-digit-appearances.py
+++ 3895-count-digit-appearances/3895-count-digit-appearances.py
@@ -1,24 +1,42 @@
 class Solution(object):
     def countDigitOccurrences(self, nums, digit):
+
         count = 0
-        target = str(digit)
 
-        for i in nums:
-            for ch in str(i):
-                if ch == target:
-                    count += 1
-        return count
-
-        '''for i in nums:
-            if i > 9:
-                rem = i % 10
-                i = i // 10
-
+        for num in nums:
+            
+            while num > 0:
+                rem = num % 10
                 if rem == digit:
                     count += 1
-
-            if i == digit:
-                count += 1
+                num = num//10
 
         return count
-        '''
+
+        
+
+#solution 1:
+
+"""count = 0
+target = str(digit)
+
+for i in nums:
+    for ch in str(i):
+        if ch == target:
+            count += 1
+return count"""
+
+#Rough workout:
+'''for i in nums:
+    if i > 9:
+        rem = i % 10
+        i = i // 10
+
+        if rem == digit:
+            count += 1
+
+    if i == digit:
+        count += 1
+
+return count
+'''
